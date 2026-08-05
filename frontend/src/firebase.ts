2676b92db47c3ce690f518435aa08757e2ed6ec3
Harsh Sharma <harshsharmarohta@gmail.com>
2026-08-05 18:37:47 +0530

    fix exact api key and prepare for vercel auth

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD4dLYMbOj9R4Vu2FZBWLOU_UlUNa7Vs_o",
+  apiKey: "AIzaSyD4dLYMbOj9R4Vu2FZBWLoU_ULuNA7vS_o",
   authDomain: "placemend-and-studytracker.firebaseapp.com",
   projectId: "placemend-and-studytracker",
   storageBucket: "placemend-and-studytracker.firebasestorage.app",
@@ -10,7 +11,6 @@ const firebaseConfig = {
   appId: "1:207434545734:web:4a867064178ebf20be6847",
   measurementId: "G-CM4RRMD96H"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
