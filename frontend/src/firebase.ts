640f4f16ae72c0b16f1baffe9c5511d1e91d3ced
Harsh Sharma <harshsharmarohta@gmail.com>
2026-08-05 15:18:12 +0530

    changing project id

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,9 +3,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD4dLYMbOj9R4Vu2FZBWLoU_ULuNA7vS_o",
-  authDomain: "placemend-and-studytracker.firebaseapp.com",
-  projectId: "placemend-and-studytracker",
-  storageBucket: "placemend-and-studytracker.firebasestorage.app",
+  authDomain: "placement-and-studytracker.firebaseapp.com",
+  projectId: "placement-and-studytracker",
+  storageBucket: "placement-and-studytracker.firebasestorage.app",
   messagingSenderId: "207434545734",
   appId: "1:207434545734:web:4a867064178ebf20be6847",
   measurementId: "G-CM4RRMD96H"
